a2927bfe6020ca737ad19882e97a7b7cc9e9c048
Satoe Sakonjo <sakonjo310@gmail.com>
2022-12-21 22:23:24 -0800

    Commit before I breake everything

--- components/Contact.js
+++ components/Contact.js
@@ -9,7 +9,92 @@ export default function Contact() {
         </svg>
       </div>
       <div className='flex flex-col items-center text-center text-navy mb-10'>
-        <h1 className='text-2xl md:text-4xl m-10 '>Talk to me!</h1>
+        <h1 className='text-2xl md:text-4xl mt-10 '>Talk to me!</h1>
+      </div>
+      <div class="night h-8">
+        <div class="star"></div>
+        <div class="star"></div>
+        <div class="star"></div>
+        <div class="star"></div>
+        <div class="star"></div>
+      </div>
+      <div className='flex justify-center items-center'>
+        <div className="w-full shadow-lg p-10 rounded-xl my-10 lg:max-w-xl">
+          <form className="mt-6">
+            <div className="mb-10">
+              <label>
+                <span className="text-gray-700">Your name</span>
+                <input
+                  type="text"
+                  name="name"
+                  className="
+                    w-full
+                    block
+                    border-gray-300
+                    rounded-md
+                    shadow-sm
+                    focus:border-[#f29b9b]
+                    focus:ring
+                    focus:ring-[#ecbcbc]
+                    focus:ring-opacity-50
+                  "
+                  placeholder="John cooks"
+                />
+              </label>
+            </div>
+            <div className="mb-10">
+              <label>
+                <span className="text-gray-700">Email address</span>
+                <input
+                  name="email"
+                  type="email"
+                  className="
+                    block
+                    w-full
+                    border-gray-300
+                    rounded-md
+                    shadow-sm
+                    focus:border-[#f29b9b]
+                    focus:ring
+                    focus:ring-[#ecbcbc]
+                    focus:ring-opacity-50
+                  "
+                  placeholder="john.cooks@example.com"
+                  required
+                />
+              </label>
+            </div>
+            <div className="mb-2">
+              <label>
+                <span class="text-gray-700">Message</span>
+                <textarea
+                  name="message"
+                  className="
+                    block
+                    w-full
+                    border-gray-300
+                    rounded-md
+                    shadow-sm
+                    focus:border-[#f29b9b]
+                    focus:ring
+                    focus:ring-[#ecbcbc]
+                    focus:ring-opacity-50
+                  "
+                  rows="5"
+                ></textarea>
+              </label>
+            </div>
+
+            <div class="mb-6 flex justify-center mt-5">
+              <button
+                type="submit"
+                className="font-semibold bg-gradient-to-r from-[#f29b9b] to-[#f76c6c] text-white px-8 py-2 rounded-md">
+                Submit
+              </button>
+            </div>
+            <div></div>
+          </form>
+        </div>
       </div>
     </div>
   )
